0b8fde5d62ff265e0f1e8eb04ca6c93aabbf9c74
pan93412 <pan93412@gmail.com>
2021-01-05 00:36:09 +0800

    feat(enums/ErrorEnums): added

---
+++ src/enums/ErrorEnums.ts
@@ -0,0 +1,7 @@
+const enum ErrorEnums {
+  TEXTAREA_CONTENT_INVAILD = '訊息過短，或是發生內部錯誤。',
+  HTTP_NOT_OK = '伺服器、API 或輸入內容有誤：HTTP 狀態碼不是 200。',
+  API_NOT_SUCCESS = 'API 或輸入內容有誤：動作未成功。'
+}
+
+export default ErrorEnums;
