44481016d7d80effa500f48db62af5a105eec5b0
pan93412 <pan93412@gmail.com>
2021-01-05 00:37:28 +0800

    feat(index): completed!

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello World from your main file!');
+/* CSS: bulma and custom css */
+import "./style/bulma.ts";
+import "./style/custom.ts";
+
+/* Main */
+import setComplainArea from './actions/setComplainArea';
+import setSlogan from './actions/setSlogan';
+import setSubmitBtn from './actions/setSubmitBtn';
+
+setComplainArea();
+setSlogan();
+setSubmitBtn();
