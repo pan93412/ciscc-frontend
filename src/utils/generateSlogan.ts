8284ceba40b0a2e56a2c9db45332cdd6c64b76f5
pan93412 <pan93412@gmail.com>
2021-01-05 00:36:40 +0800

    feat(utils/generateSlogan): added

---
+++ src/utils/generateSlogan.ts
@@ -0,0 +1,9 @@
+import generator from './generator';
+import { slogans } from '../consts';
+
+/**
+ * Generate slogans automatically.
+ */
+export default function generateSlogan(): string {
+  return generator(slogans);
+}
